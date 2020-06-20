de1d20d315ecc1b97e7fec809865a83f0e03a304
gabrielcc2 <campero@ovgu.de>
2020-06-20 23:23:34 +0200

    Missing __init__.py, for registering the proper env

--- bin_packing/__init__.py
+++ bin_packing/__init__.py
@@ -2,6 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='BinPackingEnv-v0',
-    entry_point='bin_packing.bin_packing_environment:BinPackingGymEnvironment'
+    entry_point='bin_packing.bin_packing_environment_2d:BinPackingGymEnvironment'
 )
 
